ee980e29edffe0dcc8fd215ba4da8bc1495d0fb5
LE-HU <wojciechlech1337@gmail.com>
2020-03-28 15:16:01 +0100

    final v1

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 console.log("Welcome! Thank you for viewing my website. ~ le-hu");
 
-let quoteText = document.getElementById("text");
-let quoteAuthor = document.getElementById("author");
-let newQuote = document.getElementById("new-quote");
-let twitterButton = document.getElementById("twitter-link");
+const colors = ['#ffa41b', '#000839', '#005082', '#00a8cc', '#2b580c', '#639a67', '#400082', "#fe346e", "#00bdaa", "#512b58", "#323232", "#721b65", "#b80d57", "#f8615a", "#413c69", "#4a47a3", "#ad62aa", "#5a3f11", "#9c5518", "#ef962d", "#4f3961", "#da527c", "#416788", "#53698e", "#19381f", "#7d4e57", "#485665"];
+
+const bodyElement = document.getElementById("bodyElement");
+const quoteText = document.getElementById("text");
+const quoteAuthor = document.getElementById("author");
+const newQuote = document.getElementById("new-quote");
+const twitterButton = document.getElementById("twitter-link");
+const buttonsElements = document.getElementsByClassName("button");
+
+console.log(buttonsElements);
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -12,7 +18,24 @@ function getRandomIntInclusive(min, max) {
 }
 
 window.addEventListener('DOMContentLoaded', e => {
-  let randQuote = getRandomIntInclusive(1, quotesObj.quotes.length - 1);
+
+  const randomColor = getRandomIntInclusive(0, colors.length);
+
+  bodyElement.style.backgroundColor =
+  colors[randomColor];
+  bodyElement.style.color =
+  colors[randomColor];
+  buttonsElements[0].style.backgroundColor =
+  colors[randomColor];
+  buttonsElements[1].style.backgroundColor =
+  colors[randomColor];
+  // buttonsElements.forEach((el) =>{
+  //   el.style.backgroundColor =
+  //   colors[getRandomIntInclusive(0, colors.length)];
+  // })
+
+  const randQuote = getRandomIntInclusive(1, quotesObj.quotes.length - 1);
+
   quoteText.textContent = quotesObj.quotes[randQuote]["quote"];
 
   quoteAuthor.textContent = quotesObj.quotes[randQuote]["author"];
@@ -28,7 +51,20 @@ window.addEventListener('DOMContentLoaded', e => {
 });
 
 newQuote.addEventListener('click', e => {
-  let randQuote = getRandomIntInclusive(1, quotesObj.quotes.length - 1);
+
+  const randomColor = getRandomIntInclusive(0, colors.length);
+
+  bodyElement.style.backgroundColor =
+  colors[randomColor];
+  bodyElement.style.color =
+  colors[randomColor];
+  buttonsElements[0].style.backgroundColor =
+  colors[randomColor];
+  buttonsElements[1].style.backgroundColor =
+  colors[randomColor];
+
+  const randQuote = getRandomIntInclusive(1, quotesObj.quotes.length - 1);
+
   quoteText.textContent = quotesObj.quotes[randQuote]["quote"];
 
   quoteAuthor.textContent = quotesObj.quotes[randQuote]["author"];
