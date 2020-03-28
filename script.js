d217f00c48808bfb418e6c2feab1e00954d44a70
LE-HU <wojciechlech1337@gmail.com>
2020-03-28 14:23:13 +0100

    version 0.1.0

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ console.log("Welcome! Thank you for viewing my website. ~ le-hu");
 let quoteText = document.getElementById("text");
 let quoteAuthor = document.getElementById("author");
 let newQuote = document.getElementById("new-quote");
-let twitterButton = document.getElementById("tweet-quote");
+let twitterButton = document.getElementById("twitter-link");
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
+
+window.addEventListener('DOMContentLoaded', e => {
+  let randQuote = getRandomIntInclusive(1, quotesObj.quotes.length - 1);
+  quoteText.textContent = quotesObj.quotes[randQuote]["quote"];
+
+  quoteAuthor.textContent = quotesObj.quotes[randQuote]["author"];
+
+  if (quotesObj.quotes[randQuote]["quote"].length > 180) {
+    quoteText.classList.add("even-smaller-text");
+  } else if (quotesObj.quotes[randQuote]["quote"].length > 100) {
+    quoteText.classList.add("smaller-text");
+  } else {
+    quoteText.classList.remove("smaller-text");
+    quoteText.classList.remove("even-smaller-text");
+  }
+});
 
 newQuote.addEventListener('click', e => {
   let randQuote = getRandomIntInclusive(1, quotesObj.quotes.length - 1);
