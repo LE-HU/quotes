5958a9d0f3819cf24f0ba4cbe45a2500c86042a2
LE-HU <wojciechlech1337@gmail.com>
2020-03-30 20:33:30 +0200

    animations added

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 console.log("Welcome! Thank you for viewing my quotes project. ~ le-hu");
 
-const colors = ['#804D4D', '#7F5D4C', '#7F6E4C', '#7F7F4C', '#6E7F4C', '#5D7F4C', '#4C7F4C', "#4C7F5D", "#4C7F6E", "#4C7F7F", "#4C6E7F", "#4C5D7F", "#4C4C7F", "#5D4C7F", "#6E4C7F", "#7F4C7F", "7F4C6E", "#7F4C5D"];
+
+// $("html body").animate(
+//   {
+//     backgroundColor: colors[color],
+//     color: colors[color]
+//   },
+//   1000
+// );
+// $(".button").animate(
+//   {
+//     backgroundColor: colors[color]
+//   },
+//   1000
+// );
+
+
+const colors = ['#804D4D', '#7F5D4C', '#7F6E4C', '#7F7F4C', '#6E7F4C', '#5D7F4C', '#4C7F4C', "#4C7F5D", "#4C7F6E", "#4C7F7F", "#4C6E7E", "#4C5D7F", "#4C4C7F", "#5D4C7F", "#6E4C7F", "#7F4C7F", "#7F4C6E", "#7F4C5D"];
 
 const bodyElement = document.getElementById("bodyElement");
 const quoteText = document.getElementById("text");
@@ -19,14 +35,18 @@ window.addEventListener('DOMContentLoaded', e => {
 
   const randomColor = getRandomIntInclusive(0, colors.length);
 
-  bodyElement.style.backgroundColor =
-  colors[randomColor];
-  bodyElement.style.color =
-  colors[randomColor];
-  buttonsElements[0].style.backgroundColor =
-  colors[randomColor];
-  buttonsElements[1].style.backgroundColor =
-  colors[randomColor];
+  $("body").animate({
+    backgroundColor: colors[randomColor],
+    color: colors[randomColor]
+  }, 800);
+  $("#twitter-button").animate({
+    backgroundColor: colors[randomColor]
+  }, 800);
+  $("#new-quote").animate({
+    backgroundColor: colors[randomColor]
+  }, 800);
+
+
   // buttonsElements.forEach((el) =>{
   //   el.style.backgroundColor =
   //   colors[getRandomIntInclusive(0, colors.length)];
@@ -53,14 +73,29 @@ newQuote.addEventListener('click', e => {
 
   const randomColor = getRandomIntInclusive(0, colors.length);
 
-  bodyElement.style.backgroundColor =
-  colors[randomColor];
-  bodyElement.style.color =
-  colors[randomColor];
-  buttonsElements[0].style.backgroundColor =
-  colors[randomColor];
-  buttonsElements[1].style.backgroundColor =
-  colors[randomColor];
+  $("body").animate({
+    backgroundColor: colors[randomColor],
+    color: colors[randomColor]
+  }, 800);
+  $("#twitter-button").animate({
+    backgroundColor: colors[randomColor]
+  }, 800);
+  $("#new-quote").animate({
+    backgroundColor: colors[randomColor]
+  }, 800);
+
+  // $("#text").animate({
+  //   color: '#FFF'
+  // });
+  $("#text").animate({
+    color: colors[randomColor]
+  });
+  // $("#author").animate({
+  //   color: '#FFF'
+  // });
+  $("#author").animate({
+    color: colors[randomColor]
+  });
 
   const randQuote = getRandomIntInclusive(1, quotesObj.quotes.length - 1);
 
